a489f096b09d55556f1305b3451bc9e5cdd78bbf
Renny Langat <rennylngt@gmail.com>
2024-12-08 22:07:04 +0300

    Order Details UI

--- lib/ui/views/order_details/order_details_view.dart
+++ lib/ui/views/order_details/order_details_view.dart
@@ -3,8 +3,10 @@ import 'package:beba_driver/ui/common/size_config.dart';
 import 'package:beba_driver/ui/common/text_styles.dart';
 import 'package:beba_driver/ui/common/ui_helpers.dart';
 import 'package:beba_driver/ui/views/home/home_view.dart';
+import 'package:beba_driver/ui/views/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:stacked/stacked.dart';
 
 import 'order_details_viewmodel.dart';
@@ -19,48 +21,250 @@ class OrderDetailsView extends StackedView<OrderDetailsViewModel> {
     Widget? child,
   ) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: MyColor.primary40,
-          iconTheme: const IconThemeData(color: MyColor.colorWhite),
-          title: Text(
-            "Order Details",
-            style: robotoBold.copyWith(
-                fontSize: getProportionateScreenHeight(18),
-                color: MyColor.colorWhite),
-          ),
+      appBar: AppBar(
+        backgroundColor: MyColor.primary40,
+        iconTheme: const IconThemeData(color: MyColor.colorWhite),
+        title: Text(
+          "Order Details",
+          style: robotoBold.copyWith(
+              fontSize: getProportionateScreenHeight(18),
+              color: MyColor.colorWhite),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              const OrderContainerWidget(),
-              SizedBox(
-                height: getProportionateScreenHeight(15),
-              ),
-              Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: MyColor.neutral199,
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-                    child: SvgPicture.asset(
-                      "assets/icons/calendar.svg",
-                    ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const OrderContainerWidget(),
+            SizedBox(
+              height: getProportionateScreenHeight(15),
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: MyColor.neutral199,
+                    borderRadius: BorderRadius.circular(4),
                   ),
-                  horizontalSpaceTiny,
-                  const Expanded(
-                    child: Column(
-                      children: [],
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-        ));
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                  child: SvgPicture.asset(
+                    "assets/icons/calendar.svg",
+                  ),
+                ),
+                horizontalSpaceSmall,
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Dropoff Schedule",
+                        style: robotoRegular.copyWith(
+                          fontSize: getProportionateScreenHeight(9),
+                          color: MyColor.neutral150,
+                        ),
+                      ),
+                      Text(
+                        "4th Jan, 2025 ● 10:00 AM",
+                        style: robotoRegular.copyWith(
+                          fontSize: getProportionateScreenHeight(10),
+                          color: MyColor.primary40,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Spacer(),
+                Container(
+                  decoration: BoxDecoration(
+                    color: MyColor.neutral199,
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                  child: SvgPicture.asset(
+                    "assets/icons/scale.svg",
+                  ),
+                ),
+                horizontalSpaceSmall,
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Estimated Weight",
+                        style: robotoRegular.copyWith(
+                          fontSize: getProportionateScreenHeight(9),
+                          color: MyColor.neutral150,
+                        ),
+                      ),
+                      Text(
+                        "35Kg",
+                        style: robotoRegular.copyWith(
+                          fontSize: getProportionateScreenHeight(12),
+                          color: MyColor.primary40,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            verticalSpaceMedium,
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: MyColor.neutral199,
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                  child: Icon(
+                    MdiIcons.informationOutline,
+                    color: MyColor.secondary,
+                    size: 20,
+                  ),
+                ),
+                horizontalSpaceSmall,
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Additional Information",
+                        style: robotoRegular.copyWith(
+                          fontSize: getProportionateScreenHeight(9),
+                          color: MyColor.neutral150,
+                        ),
+                      ),
+                      Text(
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                        style: robotoRegular.copyWith(
+                          fontSize: getProportionateScreenHeight(12),
+                          color: MyColor.primary40,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            verticalSpaceMedium,
+            const Divider(
+              color: MyColor.utilityOutline,
+              thickness: 1,
+            ),
+            Center(
+              child: Text(
+                "Your Bid Price",
+                style: robotoRegular.copyWith(
+                  fontSize: getProportionateScreenHeight(9),
+                  color: MyColor.neutral150,
+                ),
+              ),
+            ),
+            verticalSpaceMedium,
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: MyColor.neutral250,
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                  child: Icon(
+                    MdiIcons.minus,
+                    color: MyColor.neutral2,
+                    size: 16,
+                  ),
+                ),
+                horizontalSpaceSmall,
+                Text(
+                  "2,900",
+                  style: robotoMedium.copyWith(
+                    fontSize: getProportionateScreenHeight(20),
+                    color: MyColor.secondary,
+                  ),
+                ),
+                horizontalSpaceSmall,
+                Container(
+                  decoration: BoxDecoration(
+                    color: MyColor.neutral250,
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+                  child: Icon(
+                    MdiIcons.plus,
+                    color: MyColor.neutral2,
+                    size: 16,
+                  ),
+                ),
+              ],
+            ),
+            verticalSpaceLarge,
+            CustomButton(
+              title: "Place Bid [Kes 2,900.00]",
+              onTap: () {
+                //show Confirm Dialog
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog.adaptive(
+                      title: Text(
+                        "Confirm Bid",
+                        style: robotoBold.copyWith(
+                          fontSize: getProportionateScreenHeight(18),
+                          color: MyColor.primary40,
+                        ),
+                      ),
+                      content: Text(
+                        "Are you sure you want to place a bid of Kes 2,900.00?",
+                        style: robotoRegular.copyWith(
+                          fontSize: getProportionateScreenHeight(14),
+                          color: MyColor.neutral150,
+                        ),
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Cancel",
+                            style: robotoRegular.copyWith(
+                              fontSize: getProportionateScreenHeight(14),
+                              color: MyColor.primary40,
+                            ),
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            // Place bid logic here
+                          },
+                          child: Text(
+                            "Confirm",
+                            style: robotoBold.copyWith(
+                              fontSize: getProportionateScreenHeight(14),
+                              color: MyColor.primary40,
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   @override
--- lib/ui/views/widgets/custom_fab.dart
+++ lib/ui/views/widgets/custom_fab.dart
@@ -1,6 +1,6 @@
 import 'package:beba_driver/ui/common/app_colors.dart';
 import 'package:flutter/material.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:flutter_svg/svg.dart';
 
 class CustomFAB extends StatelessWidget {
   final Color? bgColor;
@@ -14,16 +14,27 @@ class CustomFAB extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
-      onPressed: () {},
-      backgroundColor: bgColor,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(50),
+    return Badge(
+      isLabelVisible: false,
+      // label: Icon(MdiIcons.circle, size: 10, color: MyColor.greenTextColor),
+      label: const Text(
+        "3",
+        style: TextStyle(
+          color: MyColor.greenTextColor,
+          fontSize: 10,
+          fontWeight: FontWeight.bold,
+        ),
       ),
-      child: Icon(
-        MdiIcons.plus,
-        color: MyColor.secondary,
-        size: 32,
+      backgroundColor: Colors.transparent,
+      child: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: bgColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(50),
+        ),
+        child: SvgPicture.asset(
+          "assets/icons/nav.svg",
+        ),
       ),
     );
   }
